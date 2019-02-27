ffcf18151d40df100122d156728e1a865bba352b
Hydreliox <hydreliox@gmail.com>
2019-02-27 21:27:27 +0100

    Update release to deploy on pypi

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='pyEdfEjp',
     packages=['pyEdjEjp'],  # this must be the same as the name above
     install_requires=['urllib.request'],
-    version='0.1-beta',
+    version='0.2-beta',
     description='a simple python3 library for the E.D.F. EJP Pricing',
     author='Hydreliox',
     author_email='hydreliox@gmail.com',
     url='https://github.com/HydrelioxGitHub/pyEdfEjp',  # use the URL to the github repo
-    download_url='https://github.com/HydrelioxGitHub/pyEdfEjp/tarball/0.1-beta',
+    download_url='https://github.com/HydrelioxGitHub/pyEdfEjp/tarball/0.2-beta',
     keywords=['EDF', 'EJP', 'Energy', 'API'],  # arbitrary keywords
     classifiers=[],
 )
