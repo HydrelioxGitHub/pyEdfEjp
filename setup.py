110fa755ccdfbf42eb0d294318f5df275b2399aa
Hydreliox <hydreliox@gmail.com>
2019-02-28 13:17:26 +0100

    Try changes for PyPI

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='pyEdfEjp',
-    packages=['pyEdjEjp'],  # this must be the same as the name above
+    packages=['pyEdfEjp'],  # this must be the same as the name above
     install_requires=['urllib.request'],
-    version='0.2-beta',
+    version='0.3-beta',
     description='a simple python3 library for the E.D.F. EJP Pricing',
     author='Hydreliox',
     author_email='hydreliox@gmail.com',
     url='https://github.com/HydrelioxGitHub/pyEdfEjp',  # use the URL to the github repo
-    download_url='https://github.com/HydrelioxGitHub/pyEdfEjp/tarball/0.2-beta',
+    download_url='https://github.com/HydrelioxGitHub/pyEdfEjp/tarball/0.3-beta',
     keywords=['EDF', 'EJP', 'Energy', 'API'],  # arbitrary keywords
     classifiers=[],
 )
